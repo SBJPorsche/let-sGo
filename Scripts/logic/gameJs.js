663a1dafacdcaf6c26a6551b5eaa1cab86550f8d
SBJPorsche <1107731757@qq.com>
2015-12-16 08:20:02 +0800

    1216

--- Scripts/logic/gameJs.js
+++ Scripts/logic/gameJs.js
@@ -19,6 +19,10 @@ var gameJs = qc.defineBehaviour('qc.engine.gameJs', qc.Behaviour, function() {
     self.dbarrow = null;
     self.firebase = null;
     self.sortbase = null;
+    self.lightingbase = null;    
+    self.lightingbaseGr = null;
+    self.lightingbaseR = null;
+    self.lightingbaseRGr = null;
 }, {
     // fields need to serialize
     bg1: qc.Serializer.NODE,
@@ -34,7 +38,11 @@ var gameJs = qc.defineBehaviour('qc.engine.gameJs', qc.Behaviour, function() {
     finddif: qc.Serializer.NODE,
     dbarrow: qc.Serializer.NODE,
     firebase: qc.Serializer.NODE,
-    sortbase: qc.Serializer.NODE
+    sortbase: qc.Serializer.NODE,
+    lightingbase: qc.Serializer.NODE,
+    lightingbaseGr: qc.Serializer.NODE,
+    lightingbaseR: qc.Serializer.NODE,
+    lightingbaseRGr: qc.Serializer.NODE
 });
 
 
@@ -69,100 +77,123 @@ gameJs.prototype.updateGame = function(speedgame) {
         if (self.bg2.y < -self.bg2.height) {
             self.bg2.y = self.bg2.height-10;
         }
+    }
 
-        var rigidfence = self.fence.getScript('qc.arcade.RigidBody');
-        if(self.fence.y > 0){
-             rigidfence.moves = true;
-             self.fence.y = self.fence.y - speed;
-        }else{
-             rigidfence.moves = false;
-        }
+    var rigidfence = self.fence.getScript('qc.arcade.RigidBody');
+    if(self.fence.y > 0 && G.bgRun === true){
+         rigidfence.moves = true;
+         self.fence.y = self.fence.y - speed;
+    }else{
+         rigidfence.moves = false;
+    }
 
-        var rigidbird = self.bird.getScript('qc.arcade.RigidBody');
-        if(self.bird.y > 0){
-             rigidbird.moves = true;
-             self.bird.y = self.bird.y - speed;
-        }else{
-             rigidbird.moves = false;
-        }      
-
-        var rigidarrowL = self.arrowL.getScript('qc.arcade.RigidBody');
-        if(self.arrowL.y > 0){
-             rigidarrowL.moves = true;
-             self.arrowL.y = self.arrowL.y - speed;
-        }else{
-             rigidarrowL.moves = false;
-        }    
-
-        var rigidarrowR = self.arrowR.getScript('qc.arcade.RigidBody');
-        if(self.arrowR.y > 0){
-             rigidarrowR.moves = true;
-             self.arrowR.y = self.arrowR.y - speed;
-        }else{
-             rigidarrowR.moves = false;
-        }  
-
-        var rigidkeylock = self.keylock.getScript('qc.arcade.RigidBody');
-        if(self.keylock.y > 0){
-             rigidkeylock.moves = true;
-             self.keylock.y = self.keylock.y - speed;
-        }else{
-             rigidkeylock.moves = false;
-        }   
-
-        var rigidcardbase = self.cardbase.getScript('qc.arcade.RigidBody');
-        if(self.cardbase.y > 0){
-             rigidcardbase.moves = true;
-             self.cardbase.y = self.cardbase.y - speed;
-        }else{
-             rigidcardbase.moves = false;
-        }  
-
-        var rigidmathbord = self.mathbord.getScript('qc.arcade.RigidBody');
-        if(self.mathbord.y > 0){
-             rigidmathbord.moves = true;
-             self.mathbord.y = self.mathbord.y - speed;
-        }else{
-             rigidmathbord.moves = false;
-        } 
-
-        var rigidfinddif = self.finddif.getScript('qc.arcade.RigidBody');
-        if(self.finddif.y > 0){
-             rigidfinddif.moves = true;
-             self.finddif.y = self.finddif.y - speed;
-        }else{
-             rigidfinddif.moves = false;
-        }   
-
-        var rigiddbarrow = self.dbarrow.getScript('qc.arcade.RigidBody');
-        if(self.dbarrow.y > 0){
-             rigiddbarrow.moves = true;
-             self.dbarrow.y = self.dbarrow.y - speed;
-        }else{
-             rigiddbarrow.moves = false;
-        }   
-
-        var rigidfirebase = self.firebase.getScript('qc.arcade.RigidBody');
-        if(self.firebase.y > 0){
-             rigidfirebase.moves = true;
-             self.firebase.y = self.firebase.y - speed;
-        }else{
-             rigidfirebase.moves = false;
-        }    
-
-        var rigidsortbase = self.sortbase.getScript('qc.arcade.RigidBody');
-        if(self.sortbase.y > 0){
-             rigidsortbase.moves = true;
-             self.sortbase.y = self.sortbase.y - speed;
-        }else{
-             rigidsortbase.moves = false;
-        }          
+    var rigidbird = self.bird.getScript('qc.arcade.RigidBody');
+    if(self.bird.y > 0 && G.bgRun === true){
+         rigidbird.moves = true;
+         self.bird.y = self.bird.y - speed;
+    }else{
+         rigidbird.moves = false;
     }      
+
+    var rigidarrowL = self.arrowL.getScript('qc.arcade.RigidBody');
+    if(self.arrowL.y > 0 && G.bgRun === true){
+         rigidarrowL.moves = true;
+         self.arrowL.y = self.arrowL.y - speed;
+    }else{
+         rigidarrowL.moves = false;
+    }    
+
+    var rigidarrowR = self.arrowR.getScript('qc.arcade.RigidBody');
+    if(self.arrowR.y > 0 && G.bgRun === true){
+         rigidarrowR.moves = true;
+         self.arrowR.y = self.arrowR.y - speed;
+    }else{
+         rigidarrowR.moves = false;
+    }  
+
+    var rigidkeylock = self.keylock.getScript('qc.arcade.RigidBody');
+    if(self.keylock.y > 0 && G.bgRun === true){
+         rigidkeylock.moves = true;
+         self.keylock.y = self.keylock.y - speed;
+    }else{
+         rigidkeylock.moves = false;
+    }   
+
+    var rigidcardbase = self.cardbase.getScript('qc.arcade.RigidBody');
+    if(self.cardbase.y > 0 && G.bgRun === true){
+         rigidcardbase.moves = true;
+         self.cardbase.y = self.cardbase.y - speed;
+    }else{
+         rigidcardbase.moves = false;
+    }  
+
+    var rigidmathbord = self.mathbord.getScript('qc.arcade.RigidBody');
+    if(self.mathbord.y > 0 && G.bgRun === true){
+         rigidmathbord.moves = true;
+         self.mathbord.y = self.mathbord.y - speed;
+    }else{
+         rigidmathbord.moves = false;
+    } 
+
+    var rigidfinddif = self.finddif.getScript('qc.arcade.RigidBody');
+    if(self.finddif.y > 0 && G.bgRun === true){
+         rigidfinddif.moves = true;
+         self.finddif.y = self.finddif.y - speed;
+    }else{
+         rigidfinddif.moves = false;
+    }   
+
+    var rigiddbarrow = self.dbarrow.getScript('qc.arcade.RigidBody');
+    if(self.dbarrow.y > 0 && G.bgRun === true){
+         rigiddbarrow.moves = true;
+         self.dbarrow.y = self.dbarrow.y - speed;
+    }else{
+         rigiddbarrow.moves = false;
+    }   
+
+    var rigidfirebase = self.firebase.getScript('qc.arcade.RigidBody');
+    if(self.firebase.y > 0 && G.bgRun === true){
+         rigidfirebase.moves = true;
+         self.firebase.y = self.firebase.y - speed;
+    }else{
+         rigidfirebase.moves = false;
+    }    
+
+    var rigidsortbase = self.sortbase.getScript('qc.arcade.RigidBody');
+    if(self.sortbase.y > 0 && G.bgRun === true){
+         rigidsortbase.moves = true;
+         self.sortbase.y = self.sortbase.y - speed;
+    }else{
+         rigidsortbase.moves = false;
+    }  
+
+    var rigidlightingbase = self.lightingbase.getScript('qc.arcade.RigidBody');
+    if(self.lightingbase.y > 0 && G.bgRun === true){
+         rigidlightingbase.moves = true;
+         self.lightingbase.y = self.lightingbase.y - speed;
+    }else{
+         rigidlightingbase.moves = false;
+    }
+    if(G.bgRun === true && self.lightingbase.y < 0){
+         self.lightingbase.y = -1000;
+    }
+        
+    var rigidlightingbaseR = self.lightingbaseR.getScript('qc.arcade.RigidBody');
+    if(self.lightingbaseR.y > 0 && G.bgRun === true){
+         rigidlightingbaseR.moves = true;
+         self.lightingbaseR.y = self.lightingbaseR.y - speed;
+    }else{
+         rigidlightingbaseR.moves = false;
+    }   
+    if(G.bgRun === true && self.lightingbaseR.y < 0){
+         self.lightingbaseR.y = -1000;
+    }    
+          
 };
 
 gameJs.prototype.createEnemy = function() {
     var self = this;
-    var id = G.game.math.random(0, 10);
+    var id = G.game.math.random(0, 12);
 //     id = 11;
     if(G.bgRun === true){      
         switch (id)
@@ -231,7 +262,21 @@ gameJs.prototype.createEnemy = function() {
                 self.sortbase.getScript('qc.arcade.RigidBody').addCollide(self.player);
                 self.sortbase.y = G.game.height + 1500;
             }                
-            break;                    
-        }
+            break; 
+        case 11:
+            if(self.lightingbase.y < 0){
+                self.lightingbaseGr.getScript('qc.arcade.RigidBody').addCollide(self.player);
+//                 self.lightingbaseGr.y = 30;
+                self.lightingbase.y = G.game.height + 1500;
+            }                
+            break;                
+        case 12:
+            if(self.lightingbaseR.y < 0){
+                self.lightingbaseRGr.getScript('qc.arcade.RigidBody').addCollide(self.player);
+//                 self.lightingbaseRGr.y = 30;
+                self.lightingbaseR.y = G.game.height + 1500;
+            }                
+            break;                
+        }        
     }
 };
